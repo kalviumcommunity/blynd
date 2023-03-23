060a2bfb444c3de7bdf500677e8ce2101bda22fc
Tanishqpati <tanishq.patidar@kalvium.community>
2023-03-23 11:10:46 +0530

    frontend and backend changes done

--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,7 @@ import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
-reportWebVitals();

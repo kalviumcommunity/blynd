049129389a33ae5fe06e534cd4507de90ab35bde
Tanishqpati <tanishq.patidar@kalvium.community>
2023-04-12 15:22:00 +0530

    Google authentication link in index.html, navigation flow changed in app.js

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,8 @@ import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./index.css";
 import App from "./App";
-import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <AuthContextProvider>
     <App />
-  </AuthContextProvider>
 );

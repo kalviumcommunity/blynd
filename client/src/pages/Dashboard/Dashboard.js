f3bdebea9359b105b5140ed99e9a62f1f359d918
Tanishqpati <tanishq.patidar@kalvium.community>
2023-04-17 15:20:29 +0530

    Remove db variable, secured routes, getting profiles from database, getting user info from database

--- client/src/pages/Dashboard/Dashboard.js
+++ client/src/pages/Dashboard/Dashboard.js
@@ -1,49 +1,85 @@
 import React from "react";
 import TinderCard from "react-tinder-card";
 import ReportModal from "../../components/ReportModal/ReportModal";
-
+import axios from "axios";
 import { useRef, useMemo, useEffect } from "react";
 import { useState } from "react";
+import { useCookies } from "react-cookie";
 import "./Dashboard.css";
 import Matches from "../../components/Matches/Matches";
-
-const db = [
-  {
-    name: "Ojasvi",
-    age: 21,
-    url: "https://www.scrolldroll.com/wp-content/uploads/2022/12/Hania-Amir-beautiful-pakistani-actresses.jpg",
-  },
-  {
-    name: "Ira",
-    age: 22,
-    url: "https://imgeng.jagran.com/webstories/7791/pakistani-actress-hania-amir-is-breathtaking-in-these-western-looks-1671693359.jpeg",
-  },
-  {
-    name: "Aanya",
-    age: 23,
-    url: "https://cdn.siasat.com/wp-content/uploads/2023/02/hania-amir.jpg",
-  },
-  {
-    name: "Lasya",
-    age: 24,
-    url: "https://lollywoodcity.com/wp-content/uploads/2022/03/haniaheheofficial_272875010_971176673531365_4582770850327121465_n-819x1024.jpg",
-  },
-  {
-    name: "Aakriti",
-    age: 25,
-    url: "https://m.timesofindia.com/photo/96518055/96518055.jpg",
-  },
-];
+import { useNavigate } from "react-router-dom";
+import { navigateToLoginPage } from "../../utils/routing";
 
 const Dashboard = () => {
-  const characters = db;
-  const [lastDirection, setLastDirection] = useState(0);
-  const [currentIndex, setCurrentIndex] = useState(db.length - 1);
-  const [profiles, setProfiles] = useState(null);
+  //getting users from database
+  const [user, setUser] = useState(null);
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+  const UserId = cookies.UserId;
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigateToLoginPage(navigate, cookies);
+  }, []);
+  const getUser = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL + "/user",
+        {
+          params: { UserId },
+        }
+      );
+      setUser(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const [genderedUsers, setGenderedUsers] = useState(null);
+
+  const getGenderedUsers = async () => {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_API_URL + "/gendered-users",
+        {
+          params: { gender: user?.gender_interest },
+        }
+      );
+      setGenderedUsers(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const matchedUserIds = user?.matches
+    .map(({ UserId }) => UserId)
+    .concat(UserId);
+
+  const filteredGenderedUsers = genderedUsers?.filter(
+    (genderedUser) => !matchedUserIds.includes(genderedUser.UserId)
+  );
+
+  console.log("filteredGenderedUsers ", filteredGenderedUsers);
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      getGenderedUsers();
+    }
+  }, [user]);
+  console.log(user);
+  console.log(genderedUsers);
+
+  // const characters = filteredGenderedUsers;
+  const [lastDirection, setLastDirection] = useState();
+  const [currentIndex, setCurrentIndex] = useState(
+    [filteredGenderedUsers]?.length - 1
+  );
+  // const [profiles, setProfiles] = useState(null);
   const currentIndexRef = useRef(currentIndex);
   const childRefs = useMemo(
     () =>
-      Array(db.length)
+      Array([filteredGenderedUsers]?.length)
         .fill(0)
         .map((i) => React.createRef()),
     []
@@ -53,19 +89,33 @@ const Dashboard = () => {
     setCurrentIndex(val);
     currentIndexRef.current = val;
   };
-  const canGoBack = currentIndex < db.length - 1;
+  const canGoBack = currentIndex <= [filteredGenderedUsers]?.length - 1;
 
   const canSwipe = currentIndex >= 0;
-  const swiped = (direction, nameToDelete, index) => {
+  const swiped = (direction, swipedUserId) => {
+    if (direction === "right") {
+      updateMatches(swipedUserId);
+    }
     setLastDirection(direction);
-    updateCurrentIndex(index - 1);
+  };
+
+  const updateMatches = async (matchedUserId) => {
+    try {
+      await axios.put(process.env.REACT_APP_API_URL + "/addmatch", {
+        UserId,
+        matchedUserId,
+      });
+      getUser();
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const outOfFrame = (name, idx) => {
     currentIndexRef.current >= idx && childRefs[idx].current.restoreCard();
   };
   const swipe = async (dir) => {
-    if (canSwipe && currentIndex < db.length) {
+    if (canSwipe && currentIndex < [filteredGenderedUsers].length) {
       await childRefs[currentIndex].current.swipe(dir); // Swipe the card!
     }
   };
@@ -87,8 +137,7 @@ const Dashboard = () => {
       {showReportModal && (
         <ReportModal setShowReportModal={setShowReportModal} />
       )}
-      <Matches />
-
+      {user && <Matches user={user} />}
       <div className="main-section">
         <div className="logo-filter">
           <img src="/assets/image5.png" alt="" />
@@ -102,25 +151,26 @@ const Dashboard = () => {
             !showReportModal ? "" : "swipe-hidden"
           }`}
         >
-          {characters.map((character, index) => {
+          {filteredGenderedUsers?.map((character, index) => {
             return (
               <TinderCard
                 ref={childRefs[index]}
                 className="swipe"
-                key={character.name}
-                onSwipe={(dir) => swiped(dir, character.name, index)}
-                onCardLeftScreen={() => outOfFrame(character.name, index)}
+                onSwipe={(dir) => swiped(dir, character.UserId)}
+                onCardLeftScreen={() => {
+                  outOfFrame(character.name, index);
+                }}
               >
                 <div className="profile-container">
                   <div
                     className="img-container"
-                    style={{ backgroundImage: "url(" + character.url + ")" }}
+                    style={{ backgroundImage: "url(" + character.url1 + ")" }}
                   ></div>
                   <div className="name-container">
                     <div className="name-container">
                       <div className="name">
                         <h3>
-                          {character.name}, {character.age}
+                          {character.name}, {character.dob_year}
                         </h3>
 
                         <div className="verify">
@@ -134,38 +184,6 @@ const Dashboard = () => {
               </TinderCard>
             );
           })}
-           {/* {profiles.map((character, index) => {
-            return (
-              <TinderCard
-                ref={childRefs[index]}
-                className="swipe"
-                key={character._id}
-                onSwipe={(dir) => swiped(dir, character.first_name, index)}
-                onCardLeftScreen={() => outOfFrame(character.first_name, index)}
-              >
-                <div className="profile-container">
-                  <div
-                    className="img-container"
-                    style={{ backgroundImage: "url(" + character.url1 + ")" }}
-                  ></div>
-                  <div className="name-container">
-                    <div className="name-container">
-                      <div className="name">
-                        <h3>
-                          {character.first_name}, {character.dob_year}
-                        </h3>
-
-                        <div className="verify">
-                          <img src="/assets/image 15.png" alt="" />
-                          <p>Photo verified</p>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </TinderCard>
-            );
-          })} */}
         </div>
 
         <div className="report">

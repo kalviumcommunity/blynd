c8abe5f6e4fd2c2b2feac346ab40e588ee3d9164
Tanishqpati <tanishq.patidar@kalvium.community>
2023-04-17 12:18:30 +0530

    Format changes in SwipeMatches.js

--- client/src/components/SwipeMatches/SwipeMatches.js
+++ client/src/components/SwipeMatches/SwipeMatches.js
@@ -3,7 +3,6 @@ import "./SwipeMatches.css";
 import axios from "axios";
 import { useCookies } from "react-cookie";
 
-
 const SwipeMatches = ({ matches }) => {
   const [matchedProfiles, setMatchedProfiles] = useState();
   const [cookies, setCookie, removeCookie] = useCookies(null);
@@ -12,7 +11,7 @@ const SwipeMatches = ({ matches }) => {
   const UserId = cookies.UserId;
   const getMatches = async () => {
     try {
-      const response = await axios.get(process.env.REACT_APP_API_URL, {
+      const response = await axios.get(process.env.REACT_APP_API_URL + "/users", {
         params: { userIds: JSON.stringify(matchedUserIds) },
       });
       setMatchedProfiles(response.data);
@@ -20,14 +19,14 @@ const SwipeMatches = ({ matches }) => {
       console.log(error);
     }
   };
-
+console.log(matchedProfiles)
   useEffect(() => {
     getMatches();
   }, [matches]);
 
   const filteredMatchedProfiles = matchedProfiles?.filter(
     (matchedProfile) =>
-      matchedProfile.matches.filter((profile) => profile.UserId === UserId)
+      matchedProfile.matches.filter((profile) => profile.user_id === UserId)
         .length > 0
   );
 
@@ -35,7 +34,7 @@ const SwipeMatches = ({ matches }) => {
 
   return (
     <div className="date-match-fixing">
-      { filteredMatchedProfiles && filteredMatchedProfiles.length === 0 && (
+      {filteredMatchedProfiles?.length===0 && (
         <div className="noMatchesDesign">
           <img src="/assets/nomatch-illus.svg" alt="" />
           <h3>Find your matches here</h3>
@@ -54,7 +53,7 @@ const SwipeMatches = ({ matches }) => {
           <div className="second-cont">
           {/* <h3>{match?.first_name}</h3> */}
           <p>Select a place and time</p>
-          <button >Schedule a Date</button>
+          <button>Schedule a Date</button>
 
           </div>
         </div>

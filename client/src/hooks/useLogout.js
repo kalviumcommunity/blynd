6925e6d2116f4e8e3ddfc7d2c34fe3efd0c3b464
Tanishqpati <113436940+Tanishqpati@users.noreply.github.com>
2023-04-17 13:06:18 +0530

    Revert "Formatted code, changed url of fetch and used session storage"

--- client/src/hooks/useLogout.js
+++ client/src/hooks/useLogout.js
@@ -10,25 +10,11 @@ export const useLogout = () => {
     console.log(cookies.UserId);
     removeCookie("UserId", cookies.UserId);
     removeCookie("AuthToken", cookies.AuthToken);
-    removeCookie('NID', cookies.NID)
-    sessionStorage.removeItem('user')
-    sessionStorage.removeItem('url1')
-    sessionStorage.removeItem('url2')
-    sessionStorage.removeItem('url3')
-    sessionStorage.removeItem('url4')
-    sessionStorage.removeItem('url5')
-    sessionStorage.removeItem('url6')
-    sessionStorage.removeItem('gender_interest')
-    sessionStorage.removeItem('gender_identity')
-    sessionStorage.removeItem('about')
-    sessionStorage.removeItem('dob_day')
-    sessionStorage.removeItem('dob_month')
-    sessionStorage.removeItem('dob_year')
-    sessionStorage.removeItem('height')
-    sessionStorage.removeItem('name')
-    sessionStorage.removeItem('personal_interests')
-    sessionStorage.removeItem('name')
-    navigate("/", {replace: true})
+    removeCookie("user", cookies.user);
+
+
+    navigate("/");
+    window.location.reload();
   };
 
   return { logout };

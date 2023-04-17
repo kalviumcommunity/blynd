d6ee48afa42afa001ea592e3f8310c72a03ef7e5
Tanishqpati <tanishq.patidar@kalvium.community>
2023-04-17 15:13:00 +0530

    function added for securing routes

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,10 +2,16 @@ import React, { useEffect } from 'react'
 import Navbar from '../components/Navbar/Navbar';
 import Hero from '../components/heroSection/Hero';
 import About from '../components/AboutWeb/About';
+import {useCookies} from "react-cookie";
+import {useNavigate} from "react-router-dom";
+import {navigateToDashboard} from "../utils/routing";
 
 const HomePage = () => {
-  useEffect(() => {
-    document.querySelector("html").classList.remove("overflow-hidden");
+    const navigate = useNavigate()
+    const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+    useEffect(() => {
+        navigateToDashboard(navigate, cookies)
+        document.querySelector("html").classList.remove("overflow-hidden");
   }, []);
   return (
     <>
